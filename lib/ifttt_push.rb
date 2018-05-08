bfd935e287b9c6128e946c5190d3d58ec14c1d98
Nicolas Heinig <nicolas.heinig@xing.com>
2018-05-08 13:26:29 +0200

    Get it running

--- lib/ifttt_push.rb
+++ lib/ifttt_push.rb
@@ -1,4 +1,6 @@
 require 'ifttt_push/configuration'
+require 'net/http'
+require 'json'
 
 module IftttPush
   class << self
@@ -9,7 +11,17 @@ module IftttPush
       yield(configuration)
     end
 
-    def notify
+    def notify(title:, message:, link_url:)
+      body = {
+        value1: title,
+        value2: message,
+        value3: link_url
+      }
+
+      uri = URI.parse('https://maker.ifttt.com:80/trigger/push_notification/with/key/' + self.configuration.key)
+      http = Net::HTTP.new(uri.host, uri.port)
+
+      http.post(uri.path, body.to_json, "Content-Type" => "application/json")
     end
   end
 end
--- spec/ifttt_push_spec.rb
+++ spec/ifttt_push_spec.rb
@@ -12,4 +12,26 @@ describe IftttPush do
       expect(IftttPush.configuration.key).to eq('mytestapikey')
     end
   end
+
+  describe '#notify' do
+    before do
+      stub_request(:post, %r{http://maker.ifttt.com})
+    end
+
+    subject do
+      IftttPush.notify(
+        title: 'Something happens!',
+        message: 'Hey look at this, maybe it is imporant',
+        link_url: 'http://example.com/'
+      )
+    end
+
+    it 'makes the request' do
+      subject
+
+      expect(
+        a_request(:post, %{http://maker.ifttt.com/trigger/push_notification/with/key/mytestapikey})
+      ).to have_been_made
+    end
+  end
 end
